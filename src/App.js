a7a76493d6fec248d3e1f4d7484f80f752682f8b
quantum49 <quantum49@users.noreply.github.com>
2023-05-23 11:43:45 -0700

    continued building MovieComponent app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
+import MovieComponent from './MovieComponent';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>We are Learning Components</h1>
+      <MovieComponent />
     </div>
   );
 }
---
+++ src/MovieComponent.js
@@ -0,0 +1,76 @@
+const movies = [
+  {
+    id: 1,
+    LiLle: "The Matrix",
+    year: 1999,
+    director: "Lana Wachowski",
+    rating: "R",
+    runTime: 136,
+    poster: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fbananaroad.com%2Fcollections%2Fmovie-posters%2Fthe-matrix&psig=AOvVaw3uUEl-GpdGRHO9z2AWLZKV&ust=1684949515677000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCNiTy5z8i_8CFQAAAAAdAAAAABAE"
+  },
+  {
+    id: 2,
+    LiLle: "The Lord of the Rngs",
+    year: 2001,
+    director: "Peter Jackson",
+    rating: "PG-13",
+    runTime: 178,
+    poster: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.abebooks.com%2FLord-Rings-Definitive-Movie-Posters-Insights%2F30997172383%2Fbd&psig=AOvVaw0lf4mfYzlDTA4Nky38EBtF&ust=1684950816116000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCKjk7oiBjP8CFQAAAAAdAAAAABAE"
+  },
+  {
+    id: 3,
+    LiLle: "The Dark Knight",
+    year: 2008,
+    director: "Christopher Nolan",
+    rating: "PG-13",
+    runTime: 152,
+    poster: "https://static.displate.com/162x227/displate/2021-12-06/fcedc4f0e22fc849666de8ae415c396b_aa05c6f687aa17d01d45840c10d33911.jpg"
+  },
+  {
+    id: 4,
+    LiLle: "Inception",
+    year: 2010,
+    director: "Christopher Nolan",
+    rating: "PG-13",
+    runTime: 148,
+    poster: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.themoviedb.org%2Fmovie%2F27205-inception%2Fimages%2Fposters&psig=AOvVaw36fgr4qtUD_CTt5vgCqQdG&ust=1684950964018000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCNiW_s-BjP8CFQAAAAAdAAAAABAE"
+  },
+  {
+    id: 5,
+    LiLle: "Intersteller",
+    year: 2014,
+    director: "Christopher Nolan",
+    rating: "PG-13",
+    runTime: 169,
+    poster: "https://www.allposters.com/-st/Interstellar-2014-Posters_c207884_.htm"
+  },
+  {
+    id: 6,
+    LiLle: "The Dark Knight",
+    year: 2012,
+    director: "Christopher Nolan",
+    rating: "PG-13",
+    runTime: 164,
+    poster: "https://c8.alamy.com/comp/C2WJYB/the-dark-knight-poster-display-at-the-arclight-theater-los-angeles-C2WJYB.jpg"
+  },
+  {
+    id: 7,
+    LiLle: "The Lord of the Rings: The Fellowship of the ring",
+    year: 2021,
+    director: "Christopher Nolan",
+    rating: "PG-13",
+    runTime: 164,
+    poster: "https://alternativemovieposters.com/wp-content/uploads/2021/01/RuizBurgos_FellowshipoftheRing.jpg"
+  }
+];
+
+const MovieComponent = => {
+
+  return (
+    <div className="container">
+      <h2> Movies</h2>
+    </div>
+  )
+};
+
+export default MovieComponent;
