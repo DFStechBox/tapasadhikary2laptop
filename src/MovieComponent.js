7ad9c01424aa5d933922ee54005416d1b1a94fb6
quantum49 <quantum49@users.noreply.github.com>
2023-05-23 13:23:29 -0700

    edit movie poster address

--- src/MovieComponent.js
+++ src/MovieComponent.js
@@ -1,66 +1,66 @@
 const movies = [
   {
     id: 1,
-    LiLle: "The Matrix",
+    tiLle: "The Matrix",
     year: 1999,
     director: "Lana Wachowski",
     rating: "R",
     runTime: 136,
-    poster: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fbananaroad.com%2Fcollections%2Fmovie-posters%2Fthe-matrix&psig=AOvVaw3uUEl-GpdGRHO9z2AWLZKV&ust=1684949515677000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCNiTy5z8i_8CFQAAAAAdAAAAABAE"
+    poster: ""
   },
   {
     id: 2,
-    LiLle: "The Lord of the Rngs",
+    tiLle: "The Lord of the Rngs",
     year: 2001,
     director: "Peter Jackson",
     rating: "PG-13",
     runTime: 178,
-    poster: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.abebooks.com%2FLord-Rings-Definitive-Movie-Posters-Insights%2F30997172383%2Fbd&psig=AOvVaw0lf4mfYzlDTA4Nky38EBtF&ust=1684950816116000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCKjk7oiBjP8CFQAAAAAdAAAAABAE"
+    poster: "https://m.media-amazon.com/images/I/71PfZFFz9yL._AC_UL480_FMwebp_QL65_.jpg"
   },
   {
     id: 3,
-    LiLle: "The Dark Knight",
+    title: "The Dark Knight",
     year: 2008,
     director: "Christopher Nolan",
     rating: "PG-13",
     runTime: 152,
-    poster: "https://static.displate.com/162x227/displate/2021-12-06/fcedc4f0e22fc849666de8ae415c396b_aa05c6f687aa17d01d45840c10d33911.jpg"
+    poster: "https://m.media-amazon.com/images/I/81pgKxHzBDL._AC_UL480_FMwebp_QL65_.jpg"
   },
   {
     id: 4,
-    LiLle: "Inception",
+    title: "Inception",
     year: 2010,
     director: "Christopher Nolan",
     rating: "PG-13",
     runTime: 148,
-    poster: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.themoviedb.org%2Fmovie%2F27205-inception%2Fimages%2Fposters&psig=AOvVaw36fgr4qtUD_CTt5vgCqQdG&ust=1684950964018000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCNiW_s-BjP8CFQAAAAAdAAAAABAE"
+    poster: "https://m.media-amazon.com/images/I/61S96dkqjzL._AC_UL480_FMwebp_QL65_.jpg"
   },
   {
     id: 5,
-    LiLle: "Intersteller",
+    title: "Intersteller",
     year: 2014,
     director: "Christopher Nolan",
     rating: "PG-13",
     runTime: 169,
-    poster: "https://www.allposters.com/-st/Interstellar-2014-Posters_c207884_.htm"
+    poster: "https://m.media-amazon.com/images/I/61GKC6cW3KL._AC_UL480_FMwebp_QL65_.jpg"
   },
   {
     id: 6,
-    LiLle: "The Dark Knight",
+    title: "The Dark Knight",
     year: 2012,
     director: "Christopher Nolan",
     rating: "PG-13",
     runTime: 164,
-    poster: "https://c8.alamy.com/comp/C2WJYB/the-dark-knight-poster-display-at-the-arclight-theater-los-angeles-C2WJYB.jpg"
+    poster: "https://m.media-amazon.com/images/I/81pgKxHzBDL._AC_UL480_FMwebp_QL65_.jpg"
   },
   {
     id: 7,
-    LiLle: "The Lord of the Rings: The Fellowship of the ring",
+    title: "The Lord of the Rings: The Fellowship of the ring",
     year: 2021,
     director: "Christopher Nolan",
     rating: "PG-13",
     runTime: 164,
-    poster: "https://alternativemovieposters.com/wp-content/uploads/2021/01/RuizBurgos_FellowshipoftheRing.jpg"
+    poster: "https://m.media-amazon.com/images/I/81oECPgl0kL._AC_UL480_FMwebp_QL65_.jpg"
   }
 ];
 
@@ -71,11 +71,12 @@ const MovieComponent = () => {
       <h2> Movies</h2>
       <ul className="movie-list">
         {
-          movies.map((movie) => {
+          movies.map((movie) => (
           <li className="movie" key={movie.id}>
             <img src={movie.poster} alt={movie.title} />
+            
           </li>
-          })
+          ))
         }
       </ul>
     </div>
