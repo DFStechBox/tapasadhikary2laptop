9869ecd3bb7de7d9805f68b8bb9fbda1f5274b4e
quantum49 <quantum49@users.noreply.github.com>
2023-05-24 08:52:57 -0700

    edited MovieComponent.js

--- src/MovieComponent.js
+++ src/MovieComponent.js
@@ -72,11 +72,16 @@ const MovieComponent = () => {
       <ul className="movie-list">
         {
           movies.map((movie) => (
+            
           <li className="movie" key={movie.id}>
             <img src={movie.poster} alt={movie.title} />
+              <p>{movie.title}</p> 
+              <p>by {movie.director} was released in {movie.year}</p>
+              <p>Ratings: {movie.rating}</p>
+            </li>
             
-          </li>
           ))
+          
         }
       </ul>
     </div>
