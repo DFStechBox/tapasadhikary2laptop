82f9382b76bfbd7138ee023ff5922ea5ff2480a2
quantum49 <quantum49@users.noreply.github.com>
2023-05-24 09:05:17 -0700

    Moved 'movies data' into data/movies.js

--- src/MovieComponent.js
+++ src/MovieComponent.js
@@ -1,68 +1,4 @@
-const movies = [
-  {
-    id: 1,
-    tiLle: "The Matrix",
-    year: 1999,
-    director: "Lana Wachowski",
-    rating: "R",
-    runTime: 136,
-    poster: "https://m.media-amazon.com/images/I/71PfZFFz9yL._AC_UL480_FMwebp_QL65_.jpg"
-  },
-  {
-    id: 2,
-    tiLle: "The Lord of the Rngs",
-    year: 2001,
-    director: "Peter Jackson",
-    rating: "PG-13",
-    runTime: 178,
-    poster: "https://m.media-amazon.com/images/I/81lD6R3Yu6L._AC_SY550_.jpg"
-  },
-  {
-    id: 3,
-    title: "The Dark Knight",
-    year: 2008,
-    director: "Christopher Nolan",
-    rating: "PG-13",
-    runTime: 152,
-    poster: "https://m.media-amazon.com/images/I/81pgKxHzBDL._AC_UL480_FMwebp_QL65_.jpg"
-  },
-  {
-    id: 4,
-    title: "Inception",
-    year: 2010,
-    director: "Christopher Nolan",
-    rating: "PG-13",
-    runTime: 148,
-    poster: "https://m.media-amazon.com/images/I/61S96dkqjzL._AC_UL480_FMwebp_QL65_.jpg"
-  },
-  {
-    id: 5,
-    title: "Intersteller",
-    year: 2014,
-    director: "Christopher Nolan",
-    rating: "PG-13",
-    runTime: 169,
-    poster: "https://m.media-amazon.com/images/I/61GKC6cW3KL._AC_UL480_FMwebp_QL65_.jpg"
-  },
-  {
-    id: 6,
-    title: "The Dark Knight",
-    year: 2012,
-    director: "Christopher Nolan",
-    rating: "PG-13",
-    runTime: 164,
-    poster: "https://m.media-amazon.com/images/I/81pgKxHzBDL._AC_UL480_FMwebp_QL65_.jpg"
-  },
-  {
-    id: 7,
-    title: "The Lord of the Rings: The Fellowship of the ring",
-    year: 2021,
-    director: "Christopher Nolan",
-    rating: "PG-13",
-    runTime: 164,
-    poster: "https://m.media-amazon.com/images/I/81oECPgl0kL._AC_UL480_FMwebp_QL65_.jpg"
-  }
-];
+import { movies } from './data/movies.js'
 
 const MovieComponent = () => {
 
@@ -72,9 +8,8 @@ const MovieComponent = () => {
       <ul className="movie-list">
         {
           movies.map((movie) => (
-            
-          <li className="movie" key={movie.id}>
-            <img src={movie.poster} alt={movie.title} />
+            <li className="movie" key={movie.id}>
+              <img src={movie.poster} alt={movie.title} />
               <p>{movie.title}</p> 
               <p>by {movie.director} was released in {movie.year}</p>
               <p>Ratings: {movie.rating}</p>
---
+++ src/data/movies.js
@@ -0,0 +1,65 @@
+export const movies = [
+  {
+    id: 1,
+    title: "The Matrix",
+    year: 1999,
+    director: "Lana Wachowski",
+    rating: "R",
+    runTime: 136,
+    poster: "https://m.media-amazon.com/images/I/71PfZFFz9yL._AC_UL480_FMwebp_QL65_.jpg"
+  },
+  {
+    id: 2,
+    title: "The Lord of the Rngs",
+    year: 2001,
+    director: "Peter Jackson",
+    rating: "PG-13",
+    runTime: 178,
+    poster: "https://m.media-amazon.com/images/I/81lD6R3Yu6L._AC_SY550_.jpg"
+  },
+  {
+    id: 3,
+    title: "The Dark Knight",
+    year: 2008,
+    director: "Christopher Nolan",
+    rating: "PG-13",
+    runTime: 152,
+    poster: "https://m.media-amazon.com/images/I/81pgKxHzBDL._AC_UL480_FMwebp_QL65_.jpg"
+  },
+  {
+    id: 4,
+    title: "Inception",
+    year: 2010,
+    director: "Christopher Nolan",
+    rating: "PG-13",
+    runTime: 148,
+    poster: "https://m.media-amazon.com/images/I/61S96dkqjzL._AC_UL480_FMwebp_QL65_.jpg"
+  },
+  {
+    id: 5,
+    title: "Intersteller",
+    year: 2014,
+    director: "Christopher Nolan",
+    rating: "PG-13",
+    runTime: 169,
+    poster: "https://m.media-amazon.com/images/I/61GKC6cW3KL._AC_UL480_FMwebp_QL65_.jpg"
+  },
+  {
+    id: 6,
+    title: "The Dark Knight",
+    year: 2012,
+    director: "Christopher Nolan",
+    rating: "PG-13",
+    runTime: 164,
+    poster: "https://m.media-amazon.com/images/I/81pgKxHzBDL._AC_UL480_FMwebp_QL65_.jpg"
+  },
+  {
+    id: 7,
+    title: "The Lord of the Rings: The Fellowship of the ring",
+    year: 2021,
+    director: "Christopher Nolan",
+    rating: "PG-13",
+    runTime: 164,
+    poster: "https://m.media-amazon.com/images/I/81oECPgl0kL._AC_UL480_FMwebp_QL65_.jpg"
+  }
+];
